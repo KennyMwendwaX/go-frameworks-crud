f974e06bcda456742c0b574c2b93cca062c53161
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-14 23:21:12 +0300

    Added methods to the router

--- routers/mux_router.go
+++ routers/mux_router.go
@@ -8,10 +8,10 @@ import (
 func SetUpBuiltInRouter() *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/users", handlers.CreateUser)
-	r.HandleFunc("/users", handlers.GetUsers)
-	r.HandleFunc("/users/{id:[0-9]+}", handlers.GetUser)
-	r.HandleFunc("/users/{id:[0-9]+}", handlers.UpdateUser)
+	r.HandleFunc("/users", handlers.GetUsers).Methods("GET")
+	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")
+	r.HandleFunc("/users/{id:[0-9]+}", handlers.GetUser).Methods("GET")
+	r.HandleFunc("/users/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
 
 	// Add other routes if needed
 
