f0e9a285b34bddc61ad7f58004685585544346af
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-14 23:19:38 +0300

    Added the UpdateUser route

--- routers/mux_router.go
+++ routers/mux_router.go
@@ -8,9 +8,10 @@ import (
 func SetUpBuiltInRouter() *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/users/create", handlers.CreateUser)
+	r.HandleFunc("/users", handlers.CreateUser)
 	r.HandleFunc("/users", handlers.GetUsers)
 	r.HandleFunc("/users/{id:[0-9]+}", handlers.GetUser)
+	r.HandleFunc("/users/{id:[0-9]+}", handlers.UpdateUser)
 
 	// Add other routes if needed
 
