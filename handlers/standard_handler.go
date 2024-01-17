ed70f3034e7d5a20874ace2fe4c0de9bd9937f02
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-17 08:19:59 +0300

    Added StandardUpdateUser handler

--- handlers/standard_handler.go
+++ handlers/standard_handler.go
@@ -143,3 +143,67 @@ func StandardGetUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(userJSON)
 }
+
+// UPDATE USER
+func StandardUpdateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := db.ConnectDB()
+	if err != nil {
+		log.Fatal(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Extract user ID from request URL manually
+	path := r.URL.Path
+	if path == "/users/" {
+		log.Println("User ID not provided in the URL")
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	// Trim the trailing slash and extract user ID from the path, assuming "/users/{id}/"
+	userIDStr := strings.TrimSuffix(path[len("/users/"):], "/")
+	userID, err := strconv.ParseUint(userIDStr, 10, 32)
+	if err != nil {
+		log.Println("Error converting userId to unit32:", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	var existingUser models.User
+	result := db.First(&existingUser, userID)
+	if result.Error != nil {
+		log.Println("Error fetching user from the database:", result.Error)
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	// Get form values
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	ageStr := r.FormValue("age")
+
+	// Update user fields if provided
+	if name != "" {
+		existingUser.Name = name
+	}
+
+	if email != "" {
+		existingUser.Email = email
+	}
+
+	if ageStr != "" {
+		age, err := strconv.ParseUint(ageStr, 10, 32)
+		if err != nil {
+			log.Println("Error converting age to unit32:", err)
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		existingUser.Age = uint(age)
+	}
+
+	// Save the updated user to the database
+	db.Save(&existingUser)
+
+	w.WriteHeader(http.StatusOK)
+}
