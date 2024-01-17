420d04c42a2cf95976765efa309a5d3e3c350aa9
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-17 12:17:06 +0300

    Added GET /user/delete/ http router

--- main.go
+++ main.go
@@ -18,17 +18,23 @@ func main() {
 	// Migrate the schema
 	// db.AutoMigrate(&models.User{})
 	standardRouter := routers.StandardRouter()
+	httpRouter := routers.HttpRouter()
 	muxRouter := routers.MuxRouter()
+
 	go func() {
 		log.Fatal(http.ListenAndServe(":8000", standardRouter))
 	}()
-	fmt.Println("Server running at http://localhost:8000")
+	fmt.Println("Standard router running at http://localhost:8000")
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":8001", httpRouter))
+	}()
+	fmt.Println("HttpRouter running at http://localhost:8001")
 
 	go func() {
 		log.Fatal(http.ListenAndServe(":8002", muxRouter))
 	}()
-
-	fmt.Println("Server running at http://localhost:8002")
+	fmt.Println("Mux router running at http://localhost:8002")
 
 	select {}
 }
--- routers/http_router.go
+++ routers/http_router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/kenny-mwendwa/go-restapi-crud/handlers"
 )
 
 func HttpRouter() *httprouter.Router {
-
+	r := httprouter.New()
+	r.GET("/users", handlers.HttpGetUsers)
 	return r
 }
