ac10dbbe4fe2570269cf07d20c8043c74aae8f9b
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-17 12:38:46 +0300

    Added DELETE /users/:id http router

--- routers/http_router.go
+++ routers/http_router.go
@@ -12,6 +12,7 @@ func HttpRouter() *httprouter.Router {
 	r.POST("/users", handlers.HttpCreateUser)
 	r.GET("/users/:id", handlers.HttpGetUser)
 	r.PUT("/users/:id", handlers.HttpUpdateUser)
+	r.DELETE("/users/:id", handlers.HttpDeleteUser)
 
 	return r
 }
