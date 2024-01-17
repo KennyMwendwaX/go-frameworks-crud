109b1191e02f7e0e0d26b46fd779688e4754dec1
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-17 23:32:53 +0300

    Added GET /users chi router

--- routers/chi_router.go
+++ routers/chi_router.go
@@ -1,8 +1,14 @@
 package routers
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/kenny-mwendwa/go-restapi-crud/handlers"
+)
 
 func ChiRouter() *chi.Mux {
 	r := chi.NewRouter()
 
+	r.Get("/users", handlers.ChiGetUsers)
+
+	return r
 }
