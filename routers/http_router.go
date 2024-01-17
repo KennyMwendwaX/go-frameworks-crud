24cfe0bc434e6b5affc523d3559e0ca98981d60a
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-17 12:35:32 +0300

    Added PUT /users/:id http router

--- routers/http_router.go
+++ routers/http_router.go
@@ -7,8 +7,11 @@ import (
 
 func HttpRouter() *httprouter.Router {
 	r := httprouter.New()
+
 	r.GET("/users", handlers.HttpGetUsers)
 	r.POST("/users", handlers.HttpCreateUser)
 	r.GET("/users/:id", handlers.HttpGetUser)
+	r.PUT("/users/:id", handlers.HttpUpdateUser)
+
 	return r
 }
