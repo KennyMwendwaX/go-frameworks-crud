68f8cbc9c621699d511948a8b63c3f6c01caa9ea
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-19 00:18:53 +0300

    Added EchoGetUser handler

--- handlers/echo_handler.go
+++ handlers/echo_handler.go
@@ -72,3 +72,34 @@ func EchoGetUsers(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, users)
 }
+
+// GET ONE USER
+func EchoGetUser(c echo.Context) error {
+	db, err := db.ConnectDB()
+	if err != nil {
+		log.Fatal(err.Error())
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	// Extract user ID from request URL parameters
+	userIDstr := c.Param("id")
+
+	userID, err := strconv.ParseUint(userIDstr, 10, 32)
+	if err != nil {
+		log.Println("Error converting userId to unit32:", err)
+		return c.String(http.StatusBadRequest, "Bad Request")
+	}
+
+	var user models.User
+
+	result := db.First(&user, userID)
+	if result.Error != nil {
+		log.Println("Error fetching user from the database:", result.Error)
+		return c.String(http.StatusNotFound, "User not found")
+	}
+
+	// Set Content-Type header
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	return c.JSON(http.StatusOK, user)
+}
