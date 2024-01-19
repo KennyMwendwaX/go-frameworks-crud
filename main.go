b1ec0b6f7a71e0bb667a9afa8ad33a8451f57f90
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-19 22:36:16 +0300

    Added migrate/main.go for migrations to the DB

--- main.go
+++ main.go
@@ -10,14 +10,6 @@ import (
 )
 
 func main() {
-	// db, err := db.ConnectDB()
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// Migrate the schema
-	// db.AutoMigrate(&models.User{})
 	standardRouter := routers.StandardRouter()
 	httpRouter := routers.HttpRouter()
 	muxRouter := routers.MuxRouter()
--- migrate/main.go
+++ migrate/main.go
@@ -1,13 +1,14 @@
-package db
+package main
 
 import (
 	"log"
 
+	"github.com/kenny-mwendwa/go-restapi-crud/db"
 	"github.com/kenny-mwendwa/go-restapi-crud/models"
 )
 
 func MigrateDB() {
-	db, err := ConnectDB()
+	db, err := db.ConnectDB()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
