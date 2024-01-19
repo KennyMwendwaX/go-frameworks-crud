51d0cd3587e5f3248e234615470172ff6fae6cec
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-19 22:03:34 +0300

    Used uuid to automatically generate unique ids

--- models/user.go
+++ models/user.go
@@ -1,12 +1,24 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email" gorm:"unique"`
 	Age       uint      `json:"age"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// BeforeCreate is a GORM hook that gets called before a new record is created.
+// It sets a new UUID for the ID field.
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.ID = uuid.New()
+	return nil
 }
