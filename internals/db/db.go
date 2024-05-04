fa47e28f170e38c68027892c3e07e7426011e4ce
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-05-04 20:12:59 +0300

    feat: sqlc generate

--- internals/db/db.go
+++ internals/db/db.go
@@ -2,7 +2,7 @@
 // versions:
 //   sqlc v1.26.0
 
-package models
+package db
 
 import (
 	"context"
--- internals/db/models.go
+++ internals/db/models.go
@@ -2,7 +2,7 @@
 // versions:
 //   sqlc v1.26.0
 
-package models
+package db
 
 import (
 	"github.com/jackc/pgx/v5/pgtype"
