873f1add0ce13e7e1ccc8f90c5119887a45225f7
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-05-04 08:47:59 +0300

    feat: add GetUser, GetUsers, CreateUser, UpdateUser and DeleteUser sql queries in query.sql

---
+++ internal/db/query.sql
@@ -0,0 +1,26 @@
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: GetUsers :many
+SELECT * FROM users
+ORDER BY name;
+
+-- name: CreateUser :one
+INSERT INTO users (
+    name, email, age
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
+
+-- name: UpdateUser :exec
+UPDATE users
+SET name = $2,
+    email = $3,
+    age = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
