0f90fd8a687c09f64b7074855a562d9859aac4bd
KennyMwendwaX <kennymwendwadev@gmail.com>
2025-01-09 23:35:09 +0300

    feat: refactor user handler functions to use API configuration and improve error handling

--- internal/handlers/standard_handler.go
+++ internal/handlers/standard_handler.go
@@ -1,257 +1,174 @@
 package handlers
 
 import (
-	"context"
-	"encoding/json"
-	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/KennyMwendwaX/go-frameworks-crud/internal/db"
+	"github.com/KennyMwendwaX/go-frameworks-crud/internal/config"
+	"github.com/KennyMwendwaX/go-frameworks-crud/internal/database"
+	"github.com/KennyMwendwaX/go-frameworks-crud/internal/models"
+	"github.com/KennyMwendwaX/go-frameworks-crud/internal/utils"
 )
 
 // CREATE USER
-func StandardCreateUser(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+func StandardCreateUser(cfg *config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Parse form data
+		err := r.ParseForm()
+		if err != nil {
+			utils.RespondWithError(w, http.StatusBadRequest, "Bad Request")
+			return
+		}
 
-	conn, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		// Get form values
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+		ageStr := r.FormValue("age")
+
+		// Guard clauses to check if values are empty
+		if name == "" || email == "" || ageStr == "" {
+			utils.RespondWithError(w, http.StatusBadRequest, "Bad Request: Empty values")
+			return
+		}
+
+		// Convert age to integer
+		age, err := strconv.ParseUint(ageStr, 10, 32)
+		if err != nil {
+			utils.RespondWithError(w, http.StatusBadRequest, "Bad Request")
+			return
+		}
+
+		// Create user
+		user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+			Name:  name,
+			Email: email,
+			Age:   int32(age),
+		})
+		if err != nil {
+			utils.RespondWithError(w, http.StatusInternalServerError, "Error creating user")
+			return
+		}
+
+		utils.RespondWithJSON(w, http.StatusCreated, models.FromDatabaseUser(user))
 	}
-	defer conn.Close(ctx)
-
-	query := db.New(conn)
-
-	// Parse form data
-	err = r.ParseForm()
-	if err != nil {
-		log.Println("Error parsing form data:", err)
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	// Get form values
-	name := r.FormValue("name")
-	email := r.FormValue("email")
-	ageStr := r.FormValue("age")
-
-	// Guard clauses to check if values are empty
-	if name == "" || email == "" || ageStr == "" {
-		log.Println("Empty values detected")
-		http.Error(w, "Bad Request: Empty values", http.StatusBadRequest)
-		return
-	}
-
-	// Convert age to integer
-	age, err := strconv.ParseUint(ageStr, 10, 32)
-	if err != nil {
-		log.Println("Error converting age to integer:", err)
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	if err := query.CreateUser(ctx, db.CreateUserParams{
-		Name:  name,
-		Email: email,
-		Age:   int32(age),
-	}); err != nil {
-		log.Println("Error creating user:", err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Return a success response
-	w.WriteHeader(http.StatusCreated)
 }
 
 // GET ALL USERS
-func StandardGetUsers(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+func StandardGetUsers(cfg *config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	conn, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		users, err := cfg.DB.GetUsers(r.Context())
+		if err != nil {
+			utils.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+
+		utils.RespondWithJSON(w, http.StatusOK, models.FromDatabaseUsers(users))
 	}
-	defer conn.Close(ctx)
-
-	query := db.New(conn)
-
-	users, err := query.GetUsers(ctx)
-	if err != nil {
-		log.Println("Error fetching users from the database:", err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	usersJSON, err := json.Marshal(users)
-
-	if err != nil {
-		log.Println("Error marshaling users to JSON:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Set Content-Type header
-	w.Header().Set("Content-Type", "application/json")
-
-	// Write JSON response to the client
-	w.WriteHeader(http.StatusOK)
-	w.Write(usersJSON)
 }
 
 // GET ONE USER
-func StandardGetUser(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+func StandardGetUser(cfg *config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extract user ID from request URL parameters
+		userIdStr := r.PathValue("id")
 
-	conn, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		userId, err := strconv.ParseUint(userIdStr, 10, 32)
+		if err != nil {
+			utils.RespondWithError(w, http.StatusBadRequest, "Bad Request")
+			return
+		}
+
+		// Query the DB for the user with the specified ID
+		user, err := cfg.DB.GetUser(r.Context(), int32(userId))
+		if err != nil {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+
+		utils.RespondWithJSON(w, http.StatusOK, models.FromDatabaseUser(user))
 	}
-	defer conn.Close(ctx)
-
-	query := db.New(conn)
-
-	// Extract the userId from the url parameters
-	userIdStr := r.PathValue("id")
-
-	userId, err := strconv.ParseUint(userIdStr, 10, 32)
-	if err != nil {
-		log.Println("Error converting userId to integer:", err)
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	// Query the DB for the user with the specified ID
-	user, err := query.GetUser(ctx, int32(userId))
-	if err != nil {
-		log.Println("Error fetching user from the database:", err.Error())
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	userJSON, err := json.Marshal(user)
-
-	if err != nil {
-		log.Println("Error marshaling user to JSON:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(userJSON)
 }
 
 // UPDATE USER
-func StandardUpdateUser(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+func StandardUpdateUser(cfg *config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	conn, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	defer conn.Close(ctx)
+		// Extract user ID from request URL parameters
+		userIdStr := r.PathValue("id")
 
-	query := db.New(conn)
-
-	// Extract the userId from the url parameters
-	userIdStr := r.PathValue("id")
-
-	userId, err := strconv.ParseUint(userIdStr, 10, 32)
-	if err != nil {
-		log.Println("Error converting userId to integer:", err)
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	existingUser, err := query.GetUser(ctx, int32(userId))
-	if err != nil {
-		log.Println("Error fetching user from the database:", err.Error())
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	// Get form values
-	name := r.FormValue("name")
-	email := r.FormValue("email")
-	ageStr := r.FormValue("age")
-
-	// Update user fields if provided
-	if name != "" {
-		existingUser.Name = name
-	}
-	if email != "" {
-		existingUser.Email = email
-	}
-	if ageStr != "" {
-		age, err := strconv.ParseUint(ageStr, 10, 32)
+		userId, err := strconv.ParseUint(userIdStr, 10, 32)
 		if err != nil {
-			log.Println("Error converting age to integer:", err)
-			http.Error(w, "Bad Request", http.StatusBadRequest)
+			utils.RespondWithError(w, http.StatusBadRequest, "Bad Request")
 			return
 		}
-		existingUser.Age = int32(age)
-	}
 
-	// Save the updated user to the database
-	if err := query.UpdateUser(ctx, db.UpdateUserParams{
-		ID:    existingUser.ID,
-		Name:  existingUser.Name,
-		Email: existingUser.Email,
-		Age:   existingUser.Age,
-	}); err != nil {
-		log.Println("Error updating user:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+		existingUser, err := cfg.DB.GetUser(r.Context(), int32(userId))
+		if err != nil {
+			utils.RespondWithError(w, http.StatusNotFound, "User not found")
+			return
+		}
 
-	w.WriteHeader(http.StatusOK)
+		// Get form values
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+		ageStr := r.FormValue("age")
+
+		// Update user fields if provided
+		if name != "" {
+			existingUser.Name = name
+		}
+		if email != "" {
+			existingUser.Email = email
+		}
+		if ageStr != "" {
+			age, err := strconv.ParseUint(ageStr, 10, 32)
+			if err != nil {
+				utils.RespondWithError(w, http.StatusBadRequest, "Bad Request")
+				return
+			}
+			existingUser.Age = int32(age)
+		}
+
+		// Save the updated user to the database
+		updatedUser, err := cfg.DB.UpdateUser(r.Context(), database.UpdateUserParams{
+			ID:    existingUser.ID,
+			Name:  existingUser.Name,
+			Email: existingUser.Email,
+			Age:   existingUser.Age,
+		})
+		if err != nil {
+			utils.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+
+		utils.RespondWithJSON(w, http.StatusOK, models.FromDatabaseUser(updatedUser))
+	}
 }
 
 // DELETE USER
-func StandardDeleteUser(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+func StandardDeleteUser(cfg *config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extract user ID from request URL parameters
+		userIdStr := r.PathValue("id")
 
-	conn, err := db.ConnectDB()
-	if err != nil {
-		log.Fatal(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		userId, err := strconv.ParseUint(userIdStr, 10, 32)
+		if err != nil {
+			utils.RespondWithError(w, http.StatusBadRequest, "Bad Request")
+			return
+		}
+
+		_, err = cfg.DB.GetUser(r.Context(), int32(userId))
+		if err != nil {
+			utils.RespondWithError(w, http.StatusNotFound, "User not found")
+			return
+		}
+
+		if err := cfg.DB.DeleteUser(r.Context(), int32(userId)); err != nil {
+			utils.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+
+		utils.RespondWithJSON(w, http.StatusNoContent, "Successfully deleted the user")
 	}
-	defer conn.Close(ctx)
-
-	query := db.New(conn)
-
-	// Extract the userId from the url parameters
-	userIdStr := r.PathValue("id")
-
-	userId, err := strconv.ParseUint(userIdStr, 10, 32)
-	if err != nil {
-		log.Println("Error converting userId to integer:", err)
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	_, err = query.GetUser(ctx, int32(userId))
-	if err != nil {
-		log.Println("Error fetching user from the database:", err.Error())
-		http.Error(w, "User not found", http.StatusNotFound)
-		return
-	}
-
-	if err := query.DeleteUser(ctx, int32(userId)); err != nil {
-		log.Println("Error deleting user from the database:", err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
 }
